785254ed07795087e0b0ec10d52bfb4a15297654
Ruslan Sakevych <xlionell@gmail.com>
2015-09-27 17:27:10 +0300

    Update DoubleArrayTest with new tests

--- src/test/java/ua/yandex/shad/DoubleArrayTest.java
+++ src/test/java/ua/yandex/shad/DoubleArrayTest.java
@@ -3,7 +3,33 @@ package ua.yandex.shad;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+/**
+ * Test structure
+ *
+ * /@Test
+ * public void test[UnitOfWork_StateUnderTest_ExpectedBehavior]() {
+ *     // Arrange
+ *     ...
+ *     // Act
+ *     ...
+ *     // Assert
+ * }
+ */
+
 public class DoubleArrayTest {
 
+    @Test
+    public void testDoubleArray_defaultConstructor_zeroSize() {
+        DoubleArray array = new DoubleArray();
+        int expectedSize = 1;
+        int actualSize = array.size();
 
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDoubleArray_withNegativeCapacity_exception() {
+        int capacity = -1;
+        DoubleArray array = new DoubleArray(capacity);
+    }
 }
