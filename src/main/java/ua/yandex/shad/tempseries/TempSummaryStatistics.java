0f97b4d7e37f4ac6fa5c9b4b4294af2667fb0c5c
Ruslan Sakevych <xlionell@gmail.com>
2015-09-27 22:28:49 +0300

    Add public fields to TempSummaryStatistics class

--- src/main/java/ua/yandex/shad/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/yandex/shad/tempseries/TempSummaryStatistics.java
@@ -1,5 +1,25 @@
 package ua.yandex.shad.tempseries;
 
 public class TempSummaryStatistics {
+
+    /**
+     * Average temperature from array.
+     */
+    public double avgTemp;
+
+    /**
+     * Deviation of temperatures from array.
+     */
+    public double devTemp;
+
+    /**
+     * Minimal temperature from array.
+     */
+    public double minTemp;
+
+    /**
+     * Maximal temperature from array.
+     */
+    public double maxTemp;
     
 }
